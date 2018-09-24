19a2ead32e09469191cb74d4d51fcf22463dd64a
Firman Cahyandi <hello@firmanc.com>
2018-09-24 20:07:29 +1000

    Fixed spelling mistakes

--- src/utils/objectHelper/index.js
+++ src/utils/objectHelper/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import isAllValuesValid from './isAllValuesValid';
 
 export default ({
--- src/utils/objectHelper/isAllValuesValid.js
+++ src/utils/objectHelper/isAllValuesValid.js
@@ -1,6 +1,6 @@
 // @flow
 
-export default function isAllValuesValid(fields: Object): Boolean {
+export default function isAllValuesValid(fields: Object): boolean {
   return Object.keys(fields).every(
     (key: string): Boolean => !!fields[key],
   );
