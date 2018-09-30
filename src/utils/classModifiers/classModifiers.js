a0364810f3abd0a886241b97a589d4b92930be19
Firman Cahyandi <hello@firmanc.com>
2018-09-30 19:34:16 +1000

    Fixed importing error

--- src/utils/classModifiers/classModifiers.js
+++ src/utils/classModifiers/classModifiers.js
@@ -1,7 +1,11 @@
 // @flow
 
+import isEmpty from 'lodash/isEmpty';
+
 export default function addClassModifiers(namespace: string, modifiers: Array<string>): string {
-  const classNames: Array<string> = modifiers.map((modifier: string): string => `${namespace}--${modifier}`);
+  const classNames: Array<string> = isEmpty(modifiers)
+    ? []
+    : modifiers.map((modifier: string): string => `${namespace}--${modifier}`);
 
   return classNames.join(' ');
 }
